1b53e200e7b2d794a2cd7746ef6e69304c68f9e0
kobeomseok95 <kobumssh@naver.com>
2022-06-03 21:27:49 +0900

    기능 추가

--- src/main/java/com/example/ecs/HelloController.java
+++ src/main/java/com/example/ecs/HelloController.java
@@ -1,9 +1,15 @@
 package com.example.ecs;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
 
     @GetMapping("/hello")
@@ -12,8 +18,8 @@ public class HelloController {
     }
 
     @GetMapping("/hi")
-    public String hi() {
-        return "Hi! This is ECS Sample Project.";
+    public String hi() throws UnknownHostException {
+        return "Hi! This is ECS Sample Project at " + InetAddress.getLocalHost().getHostAddress();
     }
 
     @GetMapping("/greet")
